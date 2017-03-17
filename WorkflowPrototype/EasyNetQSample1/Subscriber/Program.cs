1a7a8e5603056d15c7ddfa4b305722638e6ee759
qbasko <qbasko@gmail.com>
2017-03-17 10:52:35 +0100

    added akka.net and easyNetQ project samples

---
+++ WorkflowPrototype/EasyNetQSample1/Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿using EasyNetQ;
+using Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<TextMessage>("test", HandleTextMessage);
+
+                Console.WriteLine("Listening for messages. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
