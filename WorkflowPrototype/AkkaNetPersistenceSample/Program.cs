d67f1a4d1d76ed2e3b8704a9db8ca99a62c73fb9
qbasko <qbasko@gmail.com>
2017-03-21 11:15:29 +0100

    modified instructions

--- WorkflowPrototype/AkkaNetPersistenceSample/Program.cs
+++ WorkflowPrototype/AkkaNetPersistenceSample/Program.cs
@@ -34,35 +34,35 @@ namespace AkkaNetPersistenceSample
 
                 if (action.Contains("create1"))
                 {
-                    CreateBlock1(actorName);
+                    CreateBlock1();
                 }
                 else if (action.Contains("display1"))
                 {
-                    DisplayActor(actorName);
+                    DisplayActor();
                 }
                 else if (action.Contains("display2"))
                 {
-                    DisplayActor2(actorName);
+                    DisplayActor2();
                 }
                 else if (action.Contains("error1"))
                 {
-                    ErrorPlayer(actorName);
+                    ErrorPlayer();
                 }
                 else if (action.Contains("process1"))
                 {
-                    ProcessRiskJob(actorName);
+                    ProcessRiskJob();
                 }
                 else if (action.Contains("create2"))
-                {
-                    CreateBlock2(actorName);
+                { 
+                    CreateBlock2();
                 }
                 else if (action.Contains("error2"))
                 {
-                    ErrorPlayer2(actorName);
+                    ErrorPlayer2();
                 }
                 else if (action.Contains("process2"))
                 {
-                    ProcessRiskJob2(actorName);
+                    ProcessRiskJob2();
                 }
                 else
                 {
@@ -77,55 +77,59 @@ namespace AkkaNetPersistenceSample
             ForegroundColor = ConsoleColor.White;
 
             WriteLine("Available commands:");
-            WriteLine("<playername> create1");
-            WriteLine("<playername> display1");
-            WriteLine("<playername> error1");
-            WriteLine("<playername> process1");
+            WriteLine("create1 - create block1");
+            WriteLine("display1 - display block1");
+            WriteLine("error1 - throw exception in block1");
+            WriteLine("process1  - process block1");
+            WriteLine("create2 - create block1");
+            WriteLine("display2 - display block1");
+            WriteLine("error2 - throw exception in block1");
+            WriteLine("process2  - process block1");
         }
 
-        private static void CreateBlock1(string actorName)
+        private static void CreateBlock1()
         {
-            _scheduler.Tell(new CreateActor(actorName));
+            _scheduler.Tell(new CreateActor("Block1"));
         }
 
-        private static void CreateBlock2(string actorName)
+        private static void CreateBlock2()
         {
             var actor = _system.ActorSelection("/user/Scheduler/Block1");
-            actor.Tell(new CreateActor(actorName));
+            actor.Tell(new CreateActor("Block2"));
         }
 
-        private static void ProcessRiskJob(string actorName)
+        private static void ProcessRiskJob()
         {
-            _system.ActorSelection($"/user/Scheduler/{actorName}")
+            _system.ActorSelection("/user/Scheduler/Block1")
                   .Tell(new ProcessRiskJob());
         }
 
-        private static void ProcessRiskJob2(string actorName)
+        private static void ProcessRiskJob2()
         {
-            _system.ActorSelection($"/user/Scheduler/Block1/{actorName}")
+            _system.ActorSelection("/user/Scheduler/Block1/Block2")
                   .Tell(new ProcessRiskJob());
         }
 
-        private static void ErrorPlayer(string actorName)
+        private static void ErrorPlayer()
         {
-            _system.ActorSelection($"/user/Scheduler/{actorName}")
+            _system.ActorSelection("/user/Scheduler/Block1")
                   .Tell(new SimulateError());
         }
 
-        private static void ErrorPlayer2(string actorName)
+        private static void ErrorPlayer2()
         {
-            _system.ActorSelection($"/user/Scheduler/Block1/{actorName}")
+            _system.ActorSelection("/user/Scheduler/Block1/Block2")
                   .Tell(new SimulateError());
         }
 
-        private static void DisplayActor(string actorName)
+        private static void DisplayActor()
         {
-            _system.ActorSelection($"/user/Scheduler/{actorName}")
+            _system.ActorSelection($"/user/Scheduler/Block1")
                   .Tell(new DisplayStatus());
         }
-        private static void DisplayActor2(string actorName)
+        private static void DisplayActor2()
         {
-            _system.ActorSelection($"/user/Scheduler/Block1/{actorName}")
+            _system.ActorSelection($"/user/Scheduler/Block1/Block2")
                   .Tell(new DisplayStatus());
         }
     }
